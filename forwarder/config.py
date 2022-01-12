68979fad7192d6c19e0685999b6db30dfe6a8fec
Coder-265 <93988223+Coder-265@users.noreply.github.com>
2022-01-12 18:30:21 +0530

    Update config.py

--- forwarder/config.py
+++ forwarder/config.py
@@ -6,9 +6,10 @@ class Development(Config):
     OWNER_ID = 862271564  # Your user id
 
     # Make sure to include the '-' sign in group and channel ids.
-    FROM_CHATS = [-1001396178915]
-    FROM_CHATS = [-1001440212716]# List of chat id's to forward messages from.
-    TO_CHATS = [-1001703238624]  # List of chat id's to forward messages to.
+    FROM_CHATS = [-1001396178915 , -1001440212716]
+    FROM_CHATS = [-1001447690970 , -1001331185454]# List of chat id's to forward messages from.
+    TO_CHATS = [-1001703238624]
+    TO_CHATS = [-1001692624546]# List of chat id's to forward messages to.
 
     REMOVE_TAG = False
     WORKERS = 16
